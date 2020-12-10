eece6983a796d3384df832b45a9e22bddf58f699
GitHub <noreply@github.com>
2020-12-10 01:41:02 +0000

    4f705499 chore: update to Angular 11 (#2794)

--- esm2015/src/router_store_module.ngfactory.js
+++ esm2015/src/router_store_module.ngfactory.js
@@ -3,7 +3,7 @@
  *
  * @suppress {suspiciousCode,uselessCode,missingProperties,missingOverride,checkTypes,extraRequire}
  * tslint:disable
- */ 
+ */
 import * as i0 from "@angular/core";
 import * as i1 from "./router_store_module";
 import * as i2 from "@ngrx/store";
@@ -11,4 +11,4 @@ import * as i3 from "@angular/router";
 import * as i4 from "./serializers/base";
 var StoreRouterConnectingModuleNgFactory = i0.ɵcmf(i1.StoreRouterConnectingModule, [], function (_l) { return i0.ɵmod([i0.ɵmpd(512, i0.ComponentFactoryResolver, i0.ɵCodegenComponentFactoryResolver, [[8, []], [3, i0.ComponentFactoryResolver], i0.NgModuleRef]), i0.ɵmpd(1073742336, i1.StoreRouterConnectingModule, i1.StoreRouterConnectingModule, [i2.Store, i3.Router, i4.RouterStateSerializer, i0.ErrorHandler, i1.ROUTER_CONFIG])]); });
 export { StoreRouterConnectingModuleNgFactory as StoreRouterConnectingModuleNgFactory };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicm91dGVyX3N0b3JlX21vZHVsZS5uZ2ZhY3RvcnkuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi8uLi9tb2R1bGVzL3JvdXRlci1zdG9yZS9zcmMvcm91dGVyX3N0b3JlX21vZHVsZS5uZ2ZhY3RvcnkudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAqIGFzIGkwIGZyb20gJ0Bhbmd1bGFyL2NvcmUnO1xuaW1wb3J0ICogYXMgaTEgZnJvbSAnLi9yb3V0ZXJfc3RvcmVfbW9kdWxlJztcbmV4cG9ydCBjb25zdCBTdG9yZVJvdXRlckNvbm5lY3RpbmdNb2R1bGVOZ0ZhY3Rvcnk6aTAuTmdNb2R1bGVGYWN0b3J5PGkxLlN0b3JlUm91dGVyQ29ubmVjdGluZ01vZHVsZT4gPSAobnVsbCBhcyBhbnkpO1xudmFyIF9kZWNsMF8wOmkwLlRlbXBsYXRlUmVmPGFueT4gPSAoPGFueT4obnVsbCBhcyBhbnkpKTtcbnZhciBfZGVjbDBfMTppMC5FbGVtZW50UmVmPGFueT4gPSAoPGFueT4obnVsbCBhcyBhbnkpKTtcbiJdfQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicm91dGVyX3N0b3JlX21vZHVsZS5uZ2ZhY3RvcnkuanMiLCJzb3VyY2VSb290IjoiLi4vLi4vLi4vLi4vbW9kdWxlcy9yb3V0ZXItc3RvcmUvIiwic291cmNlcyI6WyJzcmMvcm91dGVyX3N0b3JlX21vZHVsZS5uZ2ZhY3RvcnkudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAqIGFzIGkwIGZyb20gJ0Bhbmd1bGFyL2NvcmUnO1xuaW1wb3J0ICogYXMgaTEgZnJvbSAnLi9yb3V0ZXJfc3RvcmVfbW9kdWxlJztcbmV4cG9ydCBjb25zdCBTdG9yZVJvdXRlckNvbm5lY3RpbmdNb2R1bGVOZ0ZhY3Rvcnk6aTAuTmdNb2R1bGVGYWN0b3J5PGkxLlN0b3JlUm91dGVyQ29ubmVjdGluZ01vZHVsZT4gPSAobnVsbCBhcyBhbnkpO1xudmFyIF9kZWNsMF8wOmkwLlRlbXBsYXRlUmVmPGFueT4gPSAoPGFueT4obnVsbCBhcyBhbnkpKTtcbnZhciBfZGVjbDBfMTppMC5FbGVtZW50UmVmPGFueT4gPSAoPGFueT4obnVsbCBhcyBhbnkpKTtcbiJdfQ==
